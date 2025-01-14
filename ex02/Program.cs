737289e26ca449900d6100f20e6661551874ee5d
MATHEUS NASCIMENTO GUEDES <matheus.nguedes@senacsp.edu.br>
2025-01-14 15:21:00 -0300

    primeiro commit

---
+++ ex02/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá Mundo! Sou Matheus Nascimento");
+Console.WriteLine("Estou aprendendo lógica de programação");
+
+
---
+++ ex03/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Olá Mundo! Sou Matheus Nascimento");
+Console.WriteLine("Estou estudando no senac largo 13");
+
