3a6a27d2f9fc312a3f93fc156a3dd19365ceb3fa
MATHEUS NASCIMENTO GUEDES <matheus.nguedes@senacsp.edu.br>
2025-01-14 16:55:13 -0300

    exercicio 05

--- ex01/obj/Debug/net8.0/ex01.AssemblyInfo.cs
+++ ex01/obj/Debug/net8.0/ex01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+737289e26ca449900d6100f20e6661551874ee5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e626acde5b04506e37197df9fec33e33190b31")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ex05/Program.cs
@@ -0,0 +1,15 @@
+﻿// ex05 - Solicitar dois numeros e apresentar a soma
+
+Console.WriteLine("Digite o 1 númro: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o 2 númro: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Clear();
+
+Console.WriteLine("======================");
+Console.WriteLine("===SOMA DE NÚMEROS ===");
+Console.WriteLine("======================");
+
+Console.WriteLine($"A some é: {num1 + num2} ");
