e0e626acde5b04506e37197df9fec33e33190b31
MATHEUS NASCIMENTO GUEDES <matheus.nguedes@senacsp.edu.br>
2025-01-14 16:36:55 -0300

    exercicio 04

---
+++ ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Escreva um programa que pergunte o nome de usuário e apresente nome + boas vindas
+
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu sobrenome?");
+string sobrenome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu último sobrenome?");
+string ultimosobrenome = Console.ReadLine();
+
+//Console.WriteLine("Seja bem-vindo(a)" + nome + sobrenome + ulitmosobrenome);
+Console.WriteLine($"Seja bem-vindo(a) {nome} {sobrenome} {ultimosobrenome} ");
